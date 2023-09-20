4eaf2b6f23c69c8aab46f53513fab91d7716879e
Kanimet <kanimet200127@gmail.com>
2023-09-20 20:05:25 +0300

    Created cookie, policy and about pages

---
+++ src/app/(pages)/about/page.jsx
@@ -0,0 +1,19 @@
+import fetchContent from "@/app/services/fetch";
+import Layout from "@/app/components/Layout";
+import parse from "html-react-parser";
+import '../cookies-policy/cookie.scss'
+
+const props = await fetchContent('about')
+
+export default async function Page() {
+    return (
+        <Layout
+            properties={props.properties}
+            categories={props.categories}
+        >
+            <div className="container">
+                {parse(props.content.about)}
+            </div>
+        </Layout>
+    );
+}
